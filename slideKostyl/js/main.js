de391f64375472dbb0c5b5fda9ac4a4018e5922f
UglyFront <76052317+UglyFront@users.noreply.github.com>
2020-12-23 15:40:48 +0300

    Add files via upload

---
+++ slideKostyl/js/main.js
@@ -0,0 +1,16 @@
+function one() {
+    document.getElementById('one').style.display = "none"
+    document.getElementById('two').style.display = "block"
+}
+function two() {
+    document.getElementById('two').style.display = "none"
+    document.getElementById('three').style.display = "block"
+}
+function three() {
+    document.getElementById('three').style.display = "none"
+    document.getElementById('four').style.display = "block"
+}
+function four() {
+    document.getElementById('four').style.display = "none"
+    document.getElementById('one').style.display = "block"
+}
